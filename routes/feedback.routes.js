a81272eea9d8c6e4a9e0f5559d2349668c1161aa
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2024-02-27 20:58:11 +0530

    api for piechart

--- routes/feedback.routes.js
+++ routes/feedback.routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {createFeedback,getAllFeedback} = require('../controllers/feedback.controller');
+const { createFeedback, getAllFeedback, getFeedbackDataForPiechart } = require('../controllers/feedback.controller');
 const authMiddleware = require('../middlewares/verification.middleware');
 
 router.post('/create-feedback/:eventId', authMiddleware, createFeedback);
-router.get('/get-all-feedback/:eventId',authMiddleware,getAllFeedback)
+router.get('/get-all-feedback/:eventId', authMiddleware, getAllFeedback)
+router.get('/get-feedback-data-for-piechart', authMiddleware, getFeedbackDataForPiechart)
 
 module.exports = router;
