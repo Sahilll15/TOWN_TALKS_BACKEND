c08aa37c7aaf807551f41541295307be8998d942
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2024-02-28 03:58:44 +0530

    feedback done

--- routes/poll.routes.js
+++ routes/poll.routes.js
@@ -1,27 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const {
-    createPoll,getAllPolls,getPollById,answerPolls,getAll,getanswerPollById,answerOptions,
+    createPoll, getAllPolls, getPollById, answerPolls, getAll, getanswerPollById, answerOptions,
     getAnswerbyPollId
 } = require('../controllers/polls.controller');
 const authMiddleware = require('../middlewares/verification.middleware');
 
-// Create a new poll
 router.post('/createPoll', createPoll);
 
-// Get all polls
 router.get('/getAllPolls', getAllPolls);
 
-// Get a poll by ID
 router.get('/getPollById/:id', getPollById);
 
-// Submit a vote for a specific option in a poll
-// router.post('/submit/:id/vote', submitVote);
-router.post('/answerPolls/:pollId',authMiddleware, answerPolls);
+router.post('/answerPolls/:pollId', authMiddleware, answerPolls);
 
-router.get('/getAll', getAll);  
+router.get('/getAll', getAll);
 // router.get('/getanswerPollById/:id',authMiddleware, getanswerPollById);
-router.get('/getanswerByPollId/:pollId',authMiddleware, getAnswerbyPollId);
+router.get('/getanswerByPollId/:pollId', authMiddleware, getAnswerbyPollId);
 
 
 
