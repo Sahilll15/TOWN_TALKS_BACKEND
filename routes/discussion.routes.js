41967c11b97dee214e711a2176050fd9b2fa8678
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2024-02-27 19:53:41 +0530

    new frontend updated

--- routes/discussion.routes.js
+++ routes/discussion.routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
 const { createDiscussion, getDiscussionByCommunityId } = require('../controllers/disscusion.controller')
+const authMiddleware = require('../middlewares/verification.middleware')
 
 const router = express.Router();
 
-router.post('/create/:id', createDiscussion)
-router.get('/get/:id', getDiscussionByCommunityId)
+router.post('/create/:id', authMiddleware, createDiscussion)
+router.get('/get/:id', authMiddleware, getDiscussionByCommunityId)
 
 
 module.exports = router;
